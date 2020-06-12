6d6db600a5a11b7f83815efb195af729520cf9cd
Felipe Rodrigues <sohflp@gmail.com>
2020-06-12 11:45:01 +1000

    Add dynamic control between latest and tag/release

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # docker-serverless-python
+
 Docker image with:
 - Python 3
 - NodeJS
 - NPM
 - Serverless framework
+
+## hooks (docker hub)
+
+Thinking about the automatic release/tag generation for each Python version, we implemented a hook replacing the default behaviour of the build step. The hook is available in `hooks/build` and there is a script for local tests simulating docker hub in `build-images.sh`.
+
+For more information visit the official documenation: `https://docs.docker.com/docker-hub/builds/advanced/`.
--- build-images.sh
+++ build-images.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 set -ex
 
-# Python 3.6
-docker build \
-  --tag dnxsolutions/serverless-python:3.6-dnx1 \
-  --build-arg base_image=python:3.6-slim \
-  .
+DOCKER_REPO=dnxsolutions/serverless-python
+DOCKER_TAG=dnx1.0
+IMAGE_NAME=${DOCKER_REPO}:${DOCKER_TAG}
 
-# Python 3.7
-docker build \
-  --tag dnxsolutions/serverless-python:3.7-dnx1 \
-  --build-arg base_image=python:3.7-slim \
-  .
+if [ ${DOCKER_TAG} != "latest" ]
+then
+  echo "Build specific tag/release"
 
-# Python 3.8
-docker build \
-  --tag dnxsolutions/serverless-python:3.8-dnx1 \
-  --build-arg base_image=python:3.8-slim \
-  .
+  # Python 3.6
+  docker build \
+    --build-arg base_image=python:3.6-slim \
+    -t ${DOCKER_REPO}:3.6-${DOCKER_TAG} \
+    .
+
+  # Python 3.7
+  docker build \
+    --build-arg base_image=python:3.7-slim \
+    -t ${DOCKER_REPO}:3.7-${DOCKER_TAG} \
+    .
+
+  # Python 3.8
+  docker build \
+    --build-arg base_image=python:3.8-slim \
+    -t ${DOCKER_REPO}:3.8-${DOCKER_TAG} \
+    .
+else
+  echo "Build latest"
+
+  # Python 3.8
+  docker build \
+    --build-arg base_image=python:3.8-slim \
+    -t ${IMAGE_NAME} \
+    .
+fi
