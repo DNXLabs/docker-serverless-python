d1f390351cfc1df3e7c84a252f32837c2763770e
Felipe Rodrigues <sohflp@gmail.com>
2020-06-12 10:40:06 +1000

    Adjust tag and image name

--- build-images.sh
+++ build-images.sh
@@ -3,18 +3,18 @@ set -ex
 
 # Python 3.6
 docker build \
-  --tag dnxsolutions/serverless-python:3.6 \
+  --tag dnxsolutions/serverless-python:3.6-dnx1 \
   --build-arg base_image=python:3.6-slim \
   .
 
 # Python 3.7
 docker build \
-  --tag dnxsolutions/serverless-python:3.7 \
+  --tag dnxsolutions/serverless-python:3.7-dnx1 \
   --build-arg base_image=python:3.7-slim \
   .
 
 # Python 3.8
 docker build \
-  --tag dnxsolutions/serverless-python:3.8 \
+  --tag dnxsolutions/serverless-python:3.8-dnx1 \
   --build-arg base_image=python:3.8-slim \
   .
