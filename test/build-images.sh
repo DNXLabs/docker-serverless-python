77dd71388370ccd55cbb915ff0f6e6e89483756a
Arthur Diniz <arthurbdiniz@gmail.com>
2020-06-15 22:04:57 -0300

    Adapt tests path for new arch
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- test/build-images.sh
+++ test/build-images.sh
@@ -11,19 +11,19 @@ then
 
   # Python 3.6
   docker build \
-    --build-arg base_image=python:3.6-slim \
+    -f ../3.6/Dockerfile \
     -t ${DOCKER_REPO}:3.6-${DOCKER_TAG} \
     .
 
   # Python 3.7
   docker build \
-    --build-arg base_image=python:3.7-slim \
+    -f ../3.7/Dockerfile \
     -t ${DOCKER_REPO}:3.7-${DOCKER_TAG} \
     .
 
   # Python 3.8
   docker build \
-    --build-arg base_image=python:3.8-slim \
+    -f ../3.8/Dockerfile \
     -t ${DOCKER_REPO}:3.8-${DOCKER_TAG} \
     .
 else
@@ -31,7 +31,7 @@ else
 
   # Python 3.8
   docker build \
-    --build-arg base_image=python:3.8-slim \
+    -f ../3.8/Dockerfile \
     -t ${IMAGE_NAME} \
     .
 fi
